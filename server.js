dac9c9c60101264451137fbe50956a0d09dd9c35
mjhwest89 <mjhwest89@gmail.com>
2021-11-16 18:25:02 +1030

    add uui dep

---
+++ server.js
@@ -0,0 +1,29 @@
+// import packages //
+
+//express
+//path
+//api 
+
+
+//helper methos to generating unique ids (refer topic 21 notes, pagg 45. 
+//note: use port for Heroku refer topic 26 activity 
+
+//here 
+//const uuid = ?? 
+//const PORT = USE HEROKU METHOD HERE 
+//const app = express(); --link at top. 
+
+
+
+
+//middleware for parsinf JSON and urlencoded from data 
+
+
+
+
+
+// get Route for homepage. i.e. index.html 
+
+
+
+//get route for notes . i.e. notes.html
